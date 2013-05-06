d30ec3ed49f47ceed3be23df2a3f444bfc646cba
Jon <mouseroot@gmail.com>
2013-05-06 15:40:49 -0500

    Create login_form.php

---
+++ example/views/login_form.php
@@ -0,0 +1,5 @@
+<form method="POST" action="http://localhost/AppKit/login/">
+  Username:<input type="text" name="username"><br />
+	Password:<input type="password" name="password"><br />
+	<input type="submit" value="Login">
+</form>
