9d801ce9027a9e1838f40b46f1314711c06b9b33
Jon <mouseroot@gmail.com>
2013-05-06 22:39:11 -0500

    Create login_view.php

---
+++ blog/views/login_view.php
@@ -0,0 +1,5 @@
+<form method="POST" action="http://localhost/AppKit/login/">
+  Email: <input type="text" name="email"><br />
+	Pass : <input type="text" name="pass"><br />
+	<input type="submit" value="Login">
+</form>
