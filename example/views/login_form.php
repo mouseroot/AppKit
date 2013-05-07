29a547cfd98db7c134ce93c039aaa403aaabcef8
Jon <mouseroot@gmail.com>
2013-05-06 21:18:19 -0700

    Deleted example/views/login_form.php

--- example/views/login_form.php
+++
@@ -1,5 +0,0 @@
-<form method="POST" action="http://localhost/AppKit/login/">
-  Username:<input type="text" name="username"><br />
-	Password:<input type="password" name="password"><br />
-	<input type="submit" value="Login">
-</form>
