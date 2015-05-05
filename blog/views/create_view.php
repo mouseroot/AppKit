a5b0fb27a400d39ae0d6fc3e2c21b3f8a738f7e6
Mouseroot <mouseroot@mousetech.org>
2015-05-05 14:06:33 -0500

    Delete create_view.php

--- blog/views/create_view.php
+++
@@ -1,5 +0,0 @@
-<form method="POST" action="create">
-	Title: <input type="text" name="title"><br />
-	Content: <textarea name="content"></textarea><br />
-	<input type="submit" value="Post">
-</form>
